2aac28fe660d5fef6ce3f7ffa081597419990d2e
Peter <peter.disalvo@gmail.com>
2013-03-05 19:02:01 -0800

    Multi version support

--- src/com/gbg/MainActivity.java
+++ src/com/gbg/MainActivity.java
@@ -2,20 +2,45 @@ package com.gbg;
 
 import org.apache.cordova.DroidGap;
 
+import android.annotation.TargetApi;
 import android.os.Bundle;
 
 public class MainActivity extends DroidGap {
-
+	private int retryCount = 0;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		super.loadUrl("file:///android_asset/www/index.html");
+        super.setStringProperty("loadingDialog", "Loading...");
+        super.init();
+        if(android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+            fixJellyBeanIssues();
+        }
+        super.loadUrl("file:///android_asset/www/index.html");
 	}
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.main, menu);
-//		return true;
-//	}
 
+	@TargetApi(16)
+    protected void fixJellyBeanIssues() {
+        System.out.println(super.appView.toString());
+        try {
+            super.appView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        } catch(NullPointerException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    // catch an error and if try again 1x or quit
+    @Override
+    public void onReceivedError( int errorCode, String description, String failingUrl)
+    {
+        if(retryCount < 3) {
+            retryCount++;
+            System.out.println("Connection failed, trying again. Retry Count: "+retryCount);
+            super.loadUrl("file:///android_asset/www/index.html");
+        } else {
+            System.out.println("Sorry, it failed three times so I give up.");
+            super.loadUrl("file:///android_asset/www/fail.html");
+        }
+        return;
+    }
 }
