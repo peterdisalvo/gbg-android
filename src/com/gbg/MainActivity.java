a533b01481ca987223668bb960b348e1a3c81eb4
Peter <peter.disalvo@gmail.com>
2013-03-13 15:33:48 -0700

    added splash screen

--- src/com/gbg/MainActivity.java
+++ src/com/gbg/MainActivity.java
@@ -3,23 +3,27 @@ package com.gbg;
 import org.apache.cordova.DroidGap;
 
 import android.annotation.TargetApi;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends DroidGap {
 	private static final String TAG = MainActivity.class.getName();
 	private int retryCount = 0;
+	protected Dialog mSplashDialog;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		super.setStringProperty("loadingDialog", "Loading...");
+		
+		super.setIntegerProperty("splashscreen", R.drawable.splash);
 		super.init();
 		
 		if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
 			fixJellyBeanIssues();
 		}
-		super.loadUrl("file:///android_asset/www/index.html");
+		
+		super.loadUrl("file:///android_asset/www/index.html", 5000);
 	}
 
 	@TargetApi(16)
@@ -37,7 +41,7 @@ public class MainActivity extends DroidGap {
 	public void onReceivedError(int errorCode, String description, String failingUrl) {
 		if (retryCount < 3) {
 			retryCount++;
-			Log.i(TAG, "Connection failed, trying again. Retry Count: " + retryCount);
+			Log.i(TAG, "Connection failed, trying again.  Retry Count: " + retryCount);
 			super.loadUrl("file:///android_asset/www/index.html");
 			
 		} else {
